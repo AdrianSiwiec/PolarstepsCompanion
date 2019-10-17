7df91ab65d11e748085aa86b74a39d7ca53b2db9
Adrian Siwiec <adrian.siwiec@gmail.com>
2019-10-18 01:09:33 +0200

    Initial add files

---
+++ PolarstepsCompanion/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using MetadataExtractor;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PolarstepsCompanion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private string textBlockContent = "Empty so far...";
+        public string TextBlockContent
+        {
+            get { return textBlockContent; }
+            set
+            {
+                textBlockContent = value;
+                RaisePropertyChanged("TextBlockContent");
+            }
+        }
+
+        private void RaisePropertyChanged(string propName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == true)
+            {
+                IEnumerable<MetadataExtractor.Directory> directories = ImageMetadataReader.ReadMetadata(fileDialog.FileName);
+
+                TextBlockContent = "";
+                foreach(Directory directory in directories)
+                {
+                    foreach(Tag tag in directory.Tags)
+                    {
+                        TextBlockContent += tag.ToString();
+                        TextBlockContent += "\n";
+                    }
+                    TextBlockContent += "\n";
+                }
+
+            }
+
+
+            // For future use
+            //CommonOpenFileDialog common = new CommonOpenFileDialog();
+            //common.IsFolderPicker = true;
+            //if (common.ShowDialog() == CommonFileDialogResult.Ok)
+            //{
+            //    Trace.WriteLine(common.FileName);
+            //}
+        }
+    }
+}
